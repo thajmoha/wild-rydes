2a229907c49627e1814cf521f5b1812d9fc7e0aa
tmohamad <tmohamad@parc.com>
2024-02-20 14:56:21 -0800

    added website js files

---
+++ Wild_Rydes/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  "cognito": {
+    "userPoolId": "${Token[TOKEN.21]}",
+    "userPoolClientId": "${Token[TOKEN.27]}",
+    "region": "us-west-1"
+  },
+  "api": {
+    "invokeUrl": "https://te68s8jurh.execute-api.us-west-1.amazonaws.com/prod"
+  }
+};
---
+++ infra/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  "cognito": {
+    "userPoolId": "${Token[TOKEN.22]}",
+    "userPoolClientId": "${Token[TOKEN.28]}",
+    "region": "us-west-1"
+  },
+  "api": {
+    "invokeUrl": "https://te68s8jurh.execute-api.us-west-1.amazonaws.com/prod"
+  }
+};
