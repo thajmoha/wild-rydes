3c06bf4e477e4262ce0763b6bc917a833e78d1cd
tmohamad <turki.hajmohamad@sri.com>
2024-03-03 11:37:57 -0800

    testing

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # wild-rydes
 
-a serverless ride sharing web application.
+a serverless ride web application.
 
 The application architecture uses AWS Lambda, Amazon API Gateway, Amazon DynamoDB, Amazon Cognito, and AWS S3
 
@@ -9,9 +9,14 @@ The application architecture uses AWS Lambda, Amazon API Gateway, Amazon DynamoD
 | Stack                        | AWS services        | Stack Dependencies           |
 | ---------------------------- | ------------------- | ---------------------------- |
 | WildRydesUserManagementStack | Cognito             | NA                           |
-| WildBackendStack             | API Gateway/ Lambda | WildRydesUserManagementStack |
-| WildRydesStaticWebsiteStack  | S3/ Lambda          | WildBackendStack             |
+| WildRydesBackendStack        | API Gateway/ Lambda | WildRydesUserManagementStack |
+| WildRydesStaticWebsiteStack  | S3/ Lambda          | WildRydesBackendStack        |
 
 - deploy all stacks `cdk deploy --all --profile=foothill`
 - destroy all stacks `cdk destroy --all --profile=foothill`
 - deploy <specific-stack> a specific stack `cdk deploy '<specific-stack>' --profile=foothill`
+
+## Test
+
+- to run test `npm test`
+- to regenerate a cdk template `npm test -- -u`
--- infra/test/infra.test.ts
+++ infra/test/infra.test.ts
@@ -1,13 +1,44 @@
 import * as cdk from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
-import { WebsiteS3Stack } from "../lib/website-s3-stack";
+import { StaticWebsiteStack } from "../lib/static-website-stack";
+import { UserManagementStack } from "../lib/user-management-stack";
+import { BackendStack } from "../lib/backend-stack";
 
-test("stack created", () => {
+test("stack templated should match snapshot", () => {
   const app = new cdk.App();
-  const stack = new WebsiteS3Stack(app, "MyTestStack", {
-    env: { account: "test-account", region: "us-east-1" },
-    rootDomainName: "example",
+  const appName = "test";
+  const rootDomainName = "example";
+  const env = { account: "test-account", region: "us-east-1" };
+
+  // user management stack
+  const userManagementStack = new UserManagementStack(
+    app,
+    `${appName}UserManagementStack`,
+    {
+      env,
+      appName,
+    }
+  );
+
+  // backend
+  const backendStack = new BackendStack(app, `${appName}BackendStack`, {
+    env,
+    appName,
   });
-  const template = Template.fromStack(stack);
+  backendStack.addDependency(userManagementStack);
+
+  // static website
+  const staticWebsiteStack = new StaticWebsiteStack(
+    app,
+    `${appName}StaticWebsiteStack`,
+    {
+      env,
+      rootDomainName,
+      appName,
+    }
+  );
+  staticWebsiteStack.addDependency(backendStack);
+
+  const template = Template.fromStack(staticWebsiteStack);
   expect(template.toJSON()).toMatchSnapshot();
 });
